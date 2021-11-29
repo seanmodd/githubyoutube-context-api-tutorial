cb7fe3a429aae8c066075ed3d66133b222d470e6
Sean <>
2021-11-29 15:09:56 -0800

    added the crud app to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import { CartState } from '../Context';
 import SingleProduct from './SingleProduct';
+import { Heading } from '../CRUD/components/Heading';
+import { UserList } from '../CRUD/components/UserList';
 
 const Home = () => {
   const { products } = CartState();
 
   return (
     <div style={{ textAlign: 'center' }}>
+      <Heading />
+      <UserList />
       <span style={{ fontSize: 30 }}>Products Page</span>
       <div className="productContainer">
         {products.map((prod) => (
