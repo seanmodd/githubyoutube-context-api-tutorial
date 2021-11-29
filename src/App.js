eae1aadadacb4af468bf61118229b9f3da4f130c
Sean <>
2021-11-29 15:55:19 -0800

    added the wrappers to the root of index.js instead of App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Home as CRUD } from './CRUD/components/Home';
+import { Home as CRUD } from './CRUD/components/CrudHome';
 import { AddUser } from './CRUD/components/AddUser';
 import { EditUser } from './CRUD/components/EditUser';
-import { GlobalProvider } from './CRUD/context/GlobalState';
+
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
@@ -12,22 +12,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <BrowserRouter>
-      <GlobalProvider>
-        <Header />
-        <div className="App">
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Switch>
-            <Route exact path="/crud" component={CRUD} />
-            <Route path="/add" component={AddUser} />
-            <Route path="/edit/:id" component={EditUser} />
-          </Switch>
-        </div>
-      </GlobalProvider>
+      <Header />
+      <div className="App">
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Switch>
+          <Route exact path="/crud" component={CRUD} />
+          <Route path="/add" component={AddUser} />
+          <Route path="/edit/:id" component={EditUser} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Context from './Context';
+import { GlobalProvider } from './CRUD/context/GlobalState';
 
 ReactDOM.render(
   <React.StrictMode>
     <Context>
-      <App />
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
     </Context>
   </React.StrictMode>,
   document.getElementById('root')
